f3b97e1868f0f5607d7ee6c6c10a9cff794cbabb
soyeb <soyeb@MyDesktop>
2024-05-11 01:22:51 -0400

    first commit to push social-media-project-2024

---
+++ src/main/java/com/socialmedia/socialmedia2024v1/SocialMedia2024V1Application.java
@@ -0,0 +1,13 @@
+package com.socialmedia.socialmedia2024v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMedia2024V1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMedia2024V1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialmedia/socialmedia2024v1/SocialMedia2024V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmedia.socialmedia2024v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMedia2024V1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
