183bae944ecf6e095c031dd623393331bd72066a
soyeb <soyeb@MyDesktop>
2024-05-12 14:11:55 -0400

    able to connect to databses

---
+++ src/main/java/com/socialmedia/socialmedia2024v1/controller/HealthController.java
@@ -0,0 +1,8 @@
+package com.socialmedia.socialmedia2024v1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	
+}
--- src/main/java/com/socialmedia/socialmedia2024v1/controller/SocialMedia2024V1Controller.java
+++ src/main/java/com/socialmedia/socialmedia2024v1/controller/SocialMedia2024V1Controller.java
@@ -1,8 +1,16 @@
 package com.socialmedia.socialmedia2024v1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class SocialMedia2024V1Controller {
+	
+	@GetMapping("/test")
+	public String testEndPoint() {
+		return "Hello";
+	}
 	
 }
